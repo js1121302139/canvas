52ff8c9e77ded2f7583a657d9aacc5b5982aa52b
js1121302139 <1121302139@qq.com>
2018-01-18 19:55:46 +0800

    完善方法，准备弄双人物！

--- demo.js
+++ demo.js
@@ -1,18 +1,56 @@
 var demo = {
     init:function(animName){
         var that = this;
-        utils.loadRes(
-            'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/texture.png',
-            'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/texture.js',
-            'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/skeleton.js',
+        this.loadRes(
+            // 'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/texture.png',
+            // 'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/texture.js',
+            // 'https://hiloteam.github.io/Hilo/src/extensions/dragonbones/demo/data/' + animName + '/skeleton.js',
+            './SwordsMan_tex.png',
+            './tex.js',
+            './ske.js',
             function(textureImage, textureData, skeletonData){
                 var width = 1200;
                 var height = 900;
                 var scale = 0.7;
                 that.initArmature(textureImage, textureData, skeletonData);
                 that.initForHilo(width, height, scale);
+                
             }
         );
+        console.log(this.loadRes)
+    },
+    loadScript:function(arr, callback){
+        arr = arr.forEach?arr:[arr];
+        var loadNum = arr.length;
+        arr.forEach(function(src){
+            var scriptElem = document.createElement('script');
+            scriptElem.onload = onload;
+            scriptElem.src = src;
+            document.body.appendChild(scriptElem);
+        });
+
+        function onload(){
+            loadNum --;
+            if(loadNum === 0){
+                callback && callback();
+            }
+        }
+    },
+    loadRes:function(textureImage, textureJSON, skeletonJSON, callback){
+        var that = this;
+        var loadNum = 2;
+        var onload = function(){
+            loadNum --;
+            if(loadNum === 0){
+                callback && callback(img, textureData, skeletonData);
+            }
+        };
+
+        var img = new Image;
+        img.onload = onload;
+        img.src = textureImage;
+
+        this.loadScript([textureJSON, skeletonJSON], onload);
     },
     initArmature:function(textureImage, textureData, skeletonData){
         // 新建龙骨动画
@@ -26,12 +64,13 @@ var demo = {
         // 获取龙骨动画的详细信息
         var armatureDisplay = this.armatureDisplay = armature.getDisplay();
         console.log(armatureDisplay)
+        console.log(demo)
         // 设定距离边距多少距离
-        armatureDisplay.x = demo.pos[0];
-        armatureDisplay.y = demo.pos[1];
+        armatureDisplay.x = demo.pos[0]*Math.random();
+        armatureDisplay.y = demo.pos[1]*Math.random();
         // 监听龙骨动画的启动
         armature.addEventListener(dragonBones.AnimationEvent.LOOP_COMPLETE, function(e){
-            console.log(dragonBones);
+            //console.log(dragonBones);
         }, armature);
         // 将龙骨动画添加到动画时钟里
         dragonBones.WorldClock.clock.add(armature);
@@ -54,12 +93,17 @@ var demo = {
         ticker.addTick(stage);
         ticker.addTick(dragonBones);
         ticker.start();
+        // console.log(ticker.end)
         var that = this;
         that.stage.enableDOMEvent(Hilo.event.POINTER_START,true);
         this.armatureDisplay.on(Hilo.event.POINTER_START, function(e){
             if(that.armature.animation._animationList.length > 1){
                 that.play();
             }
+            that.getAudio();            
+        })
+        this.armatureDisplay.on(Hilo.event.POINTER_END, function(e){
+            console.log('end')            
         })
     },
     play:function(){
@@ -69,7 +113,7 @@ var demo = {
     // 绑定点击事件
     bindEvent:function(){
         var that = this;
-        console.log(this)
+       // console.log(this)
         
         // window.onclick = window.ontouchstart = function(){
            
@@ -79,7 +123,23 @@ var demo = {
         this._index = this._index||0;
         var list = this.armature.animation._animationList;
         return list[(this._index++)%list.length];
+    },
+    getAudio:function(){
+        console.log('audio')
+        var that = this ;
+        // 获取音频文件
+        var audio = Hilo.WebSound.getAudio({
+            src:'./bgm1.mp3',
+            loop:false,
+            volume:1
+        }).on('load',function(e){
+            console.log(e)
+            
+        }).on('end',function(e){
+            console.log(e)
+          
+        }).play();
     }
 };
 
-demo.bindEvent();
+// demo.bindEvent();
